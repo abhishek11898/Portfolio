dd155e450faf6375b98dd6407b13dea6f1909ce3
abhishek.rana <abhishek.rana@gemsessence.com>
2024-08-16 16:24:35 +0530

    Production changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,13 +22,16 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.digest = true
+  config.assets.prefix = '/assets'
+  config.public_file_server.enabled = true
+
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
@@ -94,11 +97,13 @@ Rails.application.configure do
     port:                 587, # Use 587 for TLS/STARTTLS
     domain:               'abhishekrana.me',
     user_name:            'abhishekgugle@gmail.com',
-    password:              ENV['GMAIL_PASSWORD'],
+    password:             ENV['GMAIL_PASSWORD'], # Ensure this environment variable is set
     authentication:       'plain',
     enable_starttls_auto: true
   }
-  config.action_mailer.default_url_options = { host: 'localhost', port: 8080 }
+  
+  config.action_mailer.default_url_options = { host: 'abhishekrana.me', protocol: 'https' }
+  
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
