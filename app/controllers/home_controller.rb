3425b2adf640a6df7ffc6e1a5dca65dcff7cfb0f
abhishek.rana <abhishek.rana@gemsessence.com>
2024-08-16 16:23:49 +0530

    Remove public/assets and public/packs from tracking

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
 
   def contact_form
     contact_params = params.permit(:full_name, :email, :message)
+    binding.pry
     if ContactMailer.contact_email(contact_params[:full_name], contact_params[:email], contact_params[:message]).deliver_now
       flash[:notice] = "Thank you for your message. We'll get back to you soon."
       redirect_to root_path
